ca77c61c5f1d200065ab8351e21661490219ec97
Juhang Park <juhang3@daum.net>
2014-09-22 18:03:30 +0900

    libuv 샘플예제 추가.

---
+++ libuvEx1/libuvEx1.cpp
@@ -0,0 +1,113 @@
+﻿// libuvEx1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
+#include <uv.h>
+
+#define TEST_PORT 9000
+
+#pragma comment(lib, "libuv.lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "Psapi.lib")
+#pragma comment(lib, "Iphlpapi.lib")
+
+
+
+#define log(x) printf("%s\n", x);
+
+uv_loop_t *loop;
+
+void on_connect(uv_connect_t *req, int status);
+void on_write_end(uv_write_t *req, int status);
+uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size);
+void echo_read(uv_stream_t *server, ssize_t nread, uv_buf_t buf);
+
+// サーバからのレスポンスを表示
+void echo_read(uv_stream_t *server, ssize_t nread, uv_buf_t buf) {
+	if (nread == -1) {
+		fprintf(stderr, "error echo_read");
+		return;
+	}
+
+	// 結果を buf から取得して表示
+	printf("result: %s\n", buf.base);
+}
+
+// suggeseted_size で渡された領域を確保
+uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
+	// 読み込みのためのバッファを、サジェストされたサイズで確保
+	return uv_buf_init((char*)malloc(suggested_size), suggested_size);
+}
+
+// サーバへデータ送信後, サーバからのレスポンスを読み込む
+void on_write_end(uv_write_t *req, int status) {
+	if (status == -1) {
+		fprintf(stderr, "error on_write_end");
+		return;
+	}
+
+	// 書き込みが終わったら、すぐに読み込みを開始
+	uv_read_start(req->handle, (uv_alloc_cb)alloc_buffer, (uv_read_cb)echo_read);
+}
+
+// サーバとの接続を確立後, サーバに文字列を送信
+void on_connect(uv_connect_t *req, int status) {
+	if (status == -1) {
+		fprintf(stderr, "error on_write_end");
+		return;
+	}
+
+	// 送信メッセージを登録
+	char *message = "hello.txt";
+	int len = strlen(message);
+
+	/** これだとセグフォ
+	* uv_buf_t buf[1];
+	* buf[0].len = len;
+	* buf[0].base = message;
+	*/
+
+	// 送信データ用のバッファ
+	char buffer[100];
+	uv_buf_t buf = uv_buf_init(buffer, sizeof(buffer));
+
+	// 送信データを載せる
+	buf.len = len;
+	buf.base = message;
+
+	// ハンドルを取得
+	uv_stream_t* tcp = req->handle;
+
+	// 書き込み用構造体
+	uv_write_t write_req;
+
+	int buf_count = 1;
+	// 書き込み
+	uv_write(&write_req, tcp, &buf, buf_count, on_write_end);
+}
+
+int main(void) {
+	// loop 生成
+	loop = uv_default_loop();
+
+	// Network I/O の構造体
+	uv_tcp_t client;
+
+	// loop への登録
+	uv_tcp_init(loop, &client);
+
+	// アドレスの取得
+	struct sockaddr_in req_addr;
+	uv_ip4_addr("127.0.0.1", 9000, &req_addr);
+
+	// TCP コネクション用の構造体
+	uv_connect_t connect_req;
+
+	// 接続
+	uv_tcp_connect(&connect_req, &client, (const sockaddr*)&req_addr, on_connect);
+
+	// ループを開始
+	return uv_run(loop, UV_RUN_DEFAULT);
+}
---
+++ libuvEx2/libuvEx2.cpp
@@ -0,0 +1,226 @@
+﻿// libuvEx2.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "uv.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define TEST_PORT 9000
+
+#pragma comment(lib, "libuv.lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "Psapi.lib")
+#pragma comment(lib, "Iphlpapi.lib")
+
+
+typedef struct {
+	uv_write_t req;
+	uv_buf_t buf;
+} write_req_t;
+
+static uv_loop_t* loop;
+
+static int server_closed;
+static uv_tcp_t tcpServer;
+static uv_handle_t* server;
+
+static void after_write(uv_write_t* req, int status);
+static void after_read(uv_stream_t*, ssize_t nread, const uv_buf_t* buf);
+static void on_close(uv_handle_t* peer);
+static void on_server_close(uv_handle_t* handle);
+static void on_connection(uv_stream_t*, int status);
+
+
+static void after_write(uv_write_t* req, int status) {
+	write_req_t* wr;
+
+	/* Free the read/write buffer and the request */
+	wr = (write_req_t*)req;
+	free(wr->buf.base);
+
+	if (status == 0) {
+		free(wr);
+		return;
+	}
+
+	fprintf(stderr,
+		"uv_write error: %s - %s\n",
+		uv_err_name(status),
+		uv_strerror(status));
+
+	if (!uv_is_closing((uv_handle_t*)req->handle))
+		uv_close((uv_handle_t*)req->handle, on_close);
+	free(wr);
+}
+
+
+static void after_read(uv_stream_t* handle,
+	ssize_t nread,
+	const uv_buf_t* buf) {
+	int i;
+	write_req_t *wr;
+
+	if (nread < 0) {
+		/* Error or EOF */
+		//assert(nread == UV_EOF);
+
+		if (buf->base) {
+			free(buf->base);
+		}
+
+		uv_close((uv_handle_t*)handle, on_close);
+		return;
+	}
+
+	if (nread == 0) {
+		/* Everything OK, but nothing read. */
+		free(buf->base);
+		return;
+	}
+
+	/*
+	* Scan for the letter Q which signals that we should quit the server.
+	* If we get QS it means close the stream.
+	*/
+	if (!server_closed) {
+		for (i = 0; i < nread; i++) {
+			if (buf->base[i] == 'Q') {
+				if (i + 1 < nread && buf->base[i + 1] == 'S') {
+					free(buf->base);
+					uv_close((uv_handle_t*)handle, on_close);
+					return;
+				}
+				else {
+					uv_close(server, on_server_close);
+					server_closed = 1;
+				}
+			}
+		}
+	}
+
+	wr = (write_req_t*)malloc(sizeof *wr);
+	assert(wr != NULL);
+	wr->buf = uv_buf_init(buf->base, nread);
+
+	if (uv_write(&wr->req, handle, &wr->buf, 1, after_write)) {
+		assert(0);
+	}
+}
+
+
+static void on_close(uv_handle_t* peer) {
+	free(peer);
+}
+
+
+static void echo_alloc(uv_handle_t* handle,
+	size_t suggested_size,
+	uv_buf_t* buf) {
+	buf->base = (char*)malloc(suggested_size);
+	buf->len = suggested_size;
+}
+
+
+static void on_connection(uv_stream_t* server, int status) {
+	uv_stream_t* stream;
+	int r;
+
+	if (status != 0) {
+		fprintf(stderr, "Connect error %s\n", uv_err_name(status));
+	}
+	assert(status == 0);
+
+	stream = (uv_stream_t *)malloc(sizeof(uv_tcp_t));
+	assert(stream != NULL);
+	r = uv_tcp_init(loop, (uv_tcp_t*)stream);
+	assert(r == 0);
+
+	/* associate server with stream */
+	stream->data = server;
+
+	r = uv_accept(server, stream);
+	assert(r == 0);
+
+	r = uv_read_start(stream, echo_alloc, after_read);
+	assert(r == 0);
+}
+
+
+static void on_server_close(uv_handle_t* handle) {
+	assert(handle == server);
+}
+
+
+static void on_send(uv_udp_send_t* req, int status);
+
+
+static void on_recv(uv_udp_t* handle,
+	ssize_t nread,
+	const uv_buf_t* rcvbuf,
+	const struct sockaddr* addr,
+	unsigned flags) {
+	uv_udp_send_t* req;
+	uv_buf_t sndbuf;
+
+	assert(nread > 0);
+	assert(addr->sa_family == AF_INET);
+
+	req = (uv_udp_send_t *)malloc(sizeof(*req));
+	assert(req != NULL);
+
+	sndbuf = *rcvbuf;
+	assert(0 == uv_udp_send(req, handle, &sndbuf, 1, addr, on_send));
+}
+
+
+static void on_send(uv_udp_send_t* req, int status) {
+	assert(status == 0);
+	free(req);
+}
+
+
+static int tcp4_echo_start(int port) {
+	struct sockaddr_in addr;
+	int r;
+
+	assert(0 == uv_ip4_addr("0.0.0.0", port, &addr));
+
+	server = (uv_handle_t*)&tcpServer;
+	
+	r = uv_tcp_init(loop, &tcpServer);
+	if (r) {
+		/* TODO: Error codes */
+		fprintf(stderr, "Socket creation error\n");
+		return 1;
+	}
+
+	r = uv_tcp_bind(&tcpServer, (const struct sockaddr*) &addr, 0);
+	if (r) {
+		/* TODO: Error codes */
+		fprintf(stderr, "Bind error\n");
+		return 1;
+	}
+
+	r = uv_listen((uv_stream_t*)&tcpServer, SOMAXCONN, on_connection);
+	if (r) {
+		/* TODO: Error codes */
+		fprintf(stderr, "Listen error %s\n", uv_err_name(r));
+		return 1;
+	}
+
+	return 0;
+}
+
+
+int main(void) {
+	
+	loop = uv_default_loop();
+
+	if (tcp4_echo_start(TEST_PORT))
+		return 1;
+
+	uv_run(loop, UV_RUN_DEFAULT);
+	return 0;
+}
