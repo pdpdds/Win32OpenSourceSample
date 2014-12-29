c35043175587a43433eb8aa83027c114956ff13d
Juhang Park <juhang3@daum.net>
2014-12-29 11:47:17 +0900

    msgpack-rpc 프로젝트 예제 추가

---
+++ msgpackRPC-Ex1/msgpackRPC-Ex1.cpp
@@ -0,0 +1,54 @@
+// msgpackRPC-Ex1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <msgpack/rpc/asio.h>
+#include <boost/thread.hpp>
+
+#pragma comment(lib, "msgpack.lib")
+
+int main(int argc, char **argv)
+{
+	const static int PORT = 8070;
+
+	//클라이언트 비동기 연결
+	boost::asio::io_service client_io;
+	msgpack::rpc::asio::client client(client_io);
+	client.connect_async(boost::asio::ip::tcp::endpoint(
+		boost::asio::ip::address::from_string("127.0.0.1"), PORT));
+	boost::thread clinet_thread([&client_io](){ client_io.run(); });
+
+	//RPC 동기 호출 add 함수 호출
+	int result1;
+	std::cout << "add, 1, 2 = " << client.call_sync(&result1, "add", 1, 2) << std::endl;
+
+	//RPC 비동기 호출 mul 함수 호출
+	auto request2 = client.call_async("mul", 1.2f, 5.0f);
+
+	//비동기 호출이므로 아직 결과가 도착하지 않음
+	std::cout << *request2 << std::endl;
+
+	//결과가 올 때까지 대기한 후 결과를 얻는다.
+	float result2;
+	std::cout << "result = " << request2->sync().convert(&result2) << std::endl;
+
+	// 프라퍼티 값 설정 및 가져오기
+	{
+		auto request3 = client.call_async("set_number", 64);
+		std::cout << *request3 << std::endl;
+		request3->sync();
+	}
+	{
+		auto request3 = client.call_async("get_number");
+		request3->sync();
+		std::cout << *request3 << std::endl;
+	}
+
+	getchar();
+
+	//boost asio 종료
+	client_io.stop();
+	clinet_thread.join();
+
+	return 0;
+}
---
+++ msgpackRPC-Ex2/msgpackRPC-Ex2.cpp
@@ -0,0 +1,58 @@
+// msgpackRPC-Ex2.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <msgpack/rpc/asio.h>
+#include <boost/thread.hpp>
+
+#pragma comment(lib, "msgpack.lib")
+
+class SomeClass
+{
+	int m_number;
+
+public:
+	void setNumber(const int &number){ m_number = number; }
+	int getNumber()const{ return m_number; }
+};
+
+
+int main(int argc, char **argv)
+{
+	const static int PORT = 8070;
+
+	//디스패처에 클라이언트 요청을 처리하기 위한 핸들러 등록(add, mul)
+	//람다함수로 구현
+	msgpack::rpc::asio::dispatcher dispatcher;
+	dispatcher.add_handler("add", [](int a, int b)->int{ return a + b; });
+	dispatcher.add_handler("mul", [](float a, float b)->float{ return a*b; });
+	SomeClass s;
+	//프라퍼티의 등록. add_property를 통해서 SomeClass 인스턴스를 등록하고 내부 메소드 getNumber, setNumber 메소드를 등록한다.
+	dispatcher.add_property("number", std::function<SomeClass*()>([&s](){ return &s; })
+		, &SomeClass::getNumber
+		, &SomeClass::setNumber
+		);
+
+	//boost asio 서버 시작
+	//유저 세션이 요청해왔을 경우 디스패쳐로 해당 내용을 처리한다.
+	boost::asio::io_service server_io;
+	msgpack::rpc::asio::server server(server_io, [&dispatcher](
+		const msgpack::object &msg,
+		std::shared_ptr<msgpack::rpc::asio::session> session)
+	{
+		dispatcher.dispatch(msg, session);
+	});
+
+	//리스너 포트 8070을 개방하고 서비스를 시작한다.
+	server.listen(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT));
+	boost::thread server_thread([&server_io](){ server_io.run(); });
+
+	getchar();
+
+	server_io.stop();
+	server_thread.join();
+
+	return 0;
+}
+
+
